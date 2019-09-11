5d8c387ad9c6c8fead0ff6f9db2ea7fad0d6ebf4
willieg44@gmail.com <willieg44@gmail.com>
2019-09-11 23:04:54 +0000

    added a funtionality to the create button

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,4 +8,14 @@ class QuotesController < ApplicationController
   
     
   end
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end    
 end
